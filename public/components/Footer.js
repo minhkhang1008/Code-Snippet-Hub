2934e101047f8f65f8c5a0587582d7f9b7252014
minhkhang1008 <minhkhang.0884@gmail.com>
2025-05-10 08:50:05 +0700

    modify

--- public/components/Footer.js
+++ public/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="bg-gray-800 text-gray-400 p-6 text-center mt-auto"> {/* Changed mt-12 to mt-auto for sticky footer effect */}
-            <p>&copy; {new Date().getFullYear()} Your Name Here. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear()} Khang Nguyen (minhkhang1008). All rights reserved.</p>
             <p className="text-sm">Powered by React & Tailwind CSS. Syntax highlighting by Prism.js.</p>
         </footer>
     );
